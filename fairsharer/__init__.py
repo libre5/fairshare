e571a054bd2e597a5abaf9f134ac740ad837b9e2
Linnea Brehe <linnea.brehe@study.hs-duesseldorf.de>
2026-02-03 12:53:39 +0100

    updated name and email

--- fairsharer/__init__.py
+++ fairsharer/__init__.py
@@ -1,8 +1,9 @@
 """Documentation about fairsharer."""
+
 import logging
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
-__author__ = "Alice Bob"
-__email__ = "linnea.brehe@sudy.hs-duesseldorf.de"
+__author__ = "Linnea Brehe"
+__email__ = "linnea.brehe@study.hs-duesseldorf.de"
 __version__ = "0.1.0"
